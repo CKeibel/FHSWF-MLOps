ee9cb818f7321b9ca9cfc826fdf3fc3d9537516e
Christopher Keibel <christopher.keibel.90@gmail.com>
2025-03-15 15:02:35 +0100

    self detect feature types

--- training_server/src/datasets/adult_income_data.py
+++ training_server/src/datasets/adult_income_data.py
@@ -5,22 +5,26 @@ from sklearn.model_selection import train_test_split
 
 class AdultIncomeData(BaseDataset):
     def __init__(self, config: dict):
-        self.numerical_features: list[str] = config["columns"]["numerical_features"]
-        self.categorical_features: list[str] = config["columns"]["categorical_features"]
         self.target: str = config["columns"]["target"]
+        self.features: list[str] = config["columns"]["features"]
         df = pd.read_csv(
             config["path"],
-            names=self.numerical_features + self.categorical_features + [self.target],
+            names=self.features + [self.target],
             na_values=" ?",
             header=0,
         )
         df = df.dropna()
         df[self.target] = df[self.target].apply(lambda x: 1 if x == ">50K" else 0)
         X = df.drop(self.target, axis=1)
+        self.detect_featrue_type(X)
         y = df[self.target]
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
             X, y, test_size=0.15, random_state=42, stratify=y
         )
+
+    def detect_featrue_type(self, df: pd.DataFrame) -> dict:
+        self.numerical_features = df.select_dtypes(include=["int64", "float64"]).columns
+        self.categorical_features = df.select_dtypes(include=["object"]).columns
 
     def get_categorical_features(self) -> list[str]:
         return self.categorical_features

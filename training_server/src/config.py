c31adf2e388ad040bb40da0ca10ab7a9138bee92
Christopher Keibel <christopher.keibel.90@gmail.com>
2025-03-15 13:01:12 +0100

    add settings

---
+++ training_server/src/config.py
@@ -0,0 +1,12 @@
+
+from dynaconf import Dynaconf
+
+settings = Dynaconf(
+    envvar_prefix="DYNACONF",
+    settings_files=['settings.yaml', '.secrets.toml'],
+    environments=True,  # Enable environments like default, development, production
+    lowercase_read=True
+)
+
+# `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
+# `settings_files` = Load these files in the order.
